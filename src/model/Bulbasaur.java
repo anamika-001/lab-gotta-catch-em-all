a147d264928eb7f01393aa075577233c17a242f3
your name <ganamika063@gmail.com>
2020-03-05 09:58:51 +0530

    FSJ0220A013

--- src/model/Bulbasaur.java
+++ src/model/Bulbasaur.java
@@ -7,7 +7,7 @@ package model;
 //String type
 //String evolution
 //String baseExp
-//
+
 //Bulbasaur must extend the Pokemon class
 //Generate appropriate getters and setters
 //Generate a six argument constructor with the following arguments (pokemonName,pokemonNumber,characteristics,type,evolution,baseExp) and pokemonName and pokemonNumber should refer to the parent constructor in the Pokemon class. 
@@ -18,6 +18,60 @@ package model;
 //type = "POISON"
 //evolution = "IVYSAUR"
 //baseExp = "64" 
+
+
+public class Bulbasaur  extends Pokemon
+{
+	
+	String characteristics;
+	String type;
+	String evolution;
+	String baseExp;
+	
+
+	public String getCharacteristics() {
+		return characteristics;
+	}
+
+	public void setCharacteristics(String characteristics) {
+		this.characteristics = characteristics;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getEvolution() {
+		return evolution;
+	}
+
+	public void setEvolution(String evolution) {
+		this.evolution = evolution;
+	}
+
+	public String getBaseExp() {
+		return baseExp;
+	}
+
+	public void setBaseExp(String baseExp) 
+	{
+		this.baseExp = baseExp;
+	}
+	
+
+	public Bulbasaur(String pokemonName, int pokemonNumber, String characteristics, String type,String evolution,String baseExp) 
+	{
+		super(pokemonName, pokemonNumber);
+		this.characteristics=characteristics;
+		this.type=type;
+		this.evolution=evolution;
+		this.baseExp=baseExp;
+	}
+}
 
 
 
